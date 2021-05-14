e534e49464e38f4f8557f86cb47a26f1a5a41f12
savasan2 <83535755+savasan2@users.noreply.github.com>
2021-05-14 06:09:59 +0000

    finish3-7

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c8dcf0ecbbd75efb4ec8278eaec5bdd344d92108c04a378747fc63a20a2684cba8c936a974032aa718bece018ce9328f6f9936079c83ec49fb1df69489e0206a'
